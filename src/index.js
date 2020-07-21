c235241b62fcb1fd1e8cf930d65668e488b65cca
alivahidi78 <a_vahidi@outlook.com>
2020-07-22 01:41:57 +0430

    Fixed rerouting

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import SignIn from "./pages/signIn";
-import RoleRouter from "./roleRouter";
-import { BrowserRouter } from "react-router-dom";
-
-class Router extends React.Component {
+import FAHomePage from "./pages/FAHomePage";
+import CAHomePage from "./pages/CAHomePage";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+class MainRouter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -26,19 +26,47 @@ class Router extends React.Component {
   render() {
     // const token = localStorage.getItem("token");
     // console.log(token);
-    if (this.state.token) {
-      return <RoleRouter />;
-    } else {
-      return (
-        <BrowserRouter>
-          <SignIn setTokenAndRole={this.setTokenAndRole} />
-        </BrowserRouter>
-      );
-    }
+    return (
+      <Router>
+        <Switch>
+          <Route
+            path={"/FAHomePage"}
+            render={(props) => <FAHomePage></FAHomePage>}
+          >
+            {(() => {
+              if (!this.state.token)
+                return <Redirect to="/" />
+            })()}
+          </Route>
+          <Route
+            path={"/CAHomePage"}
+            render={(props) => <CAHomePage></CAHomePage>}
+          >
+            {(() => {
+              if (!this.state.token)
+                return <Redirect to="/" />
+            })()}
+          </Route>
+          <Route
+            path={"/"}
+            render={(props) => <SignIn setTokenAndRole={this.setTokenAndRole} />}
+          >
+            {(() => {
+              if (this.state.token) {
+                if (this.state.role === "FA")
+                  return <Redirect to="/FAHomePage" />
+                else if (this.state.role === "CA")
+                  return <Redirect to="/CAHomePage" />
+              }
+            })()}
+          </Route>
+        </Switch>
+      </Router>
+    );
   }
 }
 
-ReactDOM.render(<Router />, document.getElementById("root"));
+ReactDOM.render(<MainRouter />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
